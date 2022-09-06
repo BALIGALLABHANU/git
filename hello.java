bd60db6630d8e469b9e3b1ef6acbcaa02c903693
BALIGALLABHANU <baligallabhanu@gmail.com>
2022-09-06 11:58:27 +0530

    from dev1

--- hello.java
+++ hello.java
@@ -1,6 +1,9 @@
-class hello()
+dev1...hello.java...line (1,3,5)
+class hello # from dev1
 {
-         public static voidmain()
-              print("hello world")
-	      adding line4
- }
+        public static void main() # from dev1
+        {
+        sys.cut.printin("Hello from dev1")
+        }
+} 
+
